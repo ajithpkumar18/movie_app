90fba59d19c4ffcefb7d728f99450cc53f259d1f
Ajith <ajithpkumar231@gmail.com>
2025-08-25 22:49:31 +0530

    searched movies

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,17 +5,19 @@ import { Image, TextInput, View } from 'react-native';
 // https://api.themoviedb.org/3/movie/550?api_key=9a64a5815cb083f55d598f8b6e8f0360
 
 interface Props {
-    onPress: () => void;
-    placeholder: string;
+    onPress?: () => void;
+    placeholder?: string;
+    value?: string;
+    onChangeText?: (text: string) => void
 }
-const SearchBar = ({ onPress, placeholder }: Props) => {
+const SearchBar = ({ onPress, placeholder, value, onChangeText }: Props) => {
     return (
         <View className='flex-row items-center bg-dark-200 rounded-full px-5 py-4 border-dashed border-white'>
             <Image source={icons.search} className='size-5' resizeMode='contain' tintColor={'#ab8bff'} />
             <TextInput onPress={onPress}
                 placeholder={placeholder}
-                value=''
-                onChangeText={() => { }}
+                value={value}
+                onChangeText={onChangeText}
                 placeholderTextColor={"#a8b5db"}
                 className='flex-1 ml-2 text-white' />
         </View>
